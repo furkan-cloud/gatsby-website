0c18d8deeea23240145cc78217947068b35c7c41
furkan-cloud <firstinfirstout2020@gmail.com>
2021-05-30 13:15:29 +0300

    header created, unneccessary files deleted

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,84 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import { FaBars } from "react-icons/fa"
+import { MenuData } from "../data/MenuData"
+import { Button } from "./Button"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORE</NavLink>
+      <Bars />
+      <NavMenu>
+        {MenuData.map((item, index) => (
+          <NavLink to={item.link} key={index}>
+            {item.title}
+          </NavLink>
+        ))}
+        <NavBtn>
+          <Button round="true" primary="true" to="/trips">
+            Book a Flight
+          </Button>
+        </NavBtn>
+      </NavMenu>
+    </Nav>
+  )
 }
 
 export default Header
+
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+
+const NavLink = styled(Link)`
+  padding: 0 1rem;
+  text-decoration: none;
+  color: white;
+  display: flex;
+  height: 100%;
+  cursor: pointer;
+  align-items: center;
+`
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: white;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    cursor: pointer;
+    font-size: 1.8rem;
+  }
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -40px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
---
+++ src/data/MenuData.js
@@ -0,0 +1,18 @@
+export const MenuData = [
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Trips",
+    link: "/trips",
+  },
+  {
+    title: "Careers",
+    link: "/careees",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+]
